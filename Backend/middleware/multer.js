cf8a8280985d2de033121c6b8655d63f8b325b12
Mayank <mayankmchandratre@gmail.com>
2025-01-12 00:12:16 +0530

    multer memory STorage

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,11 +1,6 @@
 import multer from 'multer';
-// Configure multer for PDF file uploads
-export const storage = multer.diskStorage({
-  destination: 'uploads/',
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
-});
+
+
 
 export const upload = multer({
   storage: multer.memoryStorage(),
