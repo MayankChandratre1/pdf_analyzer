25f65df0992249bb5f96e0ae4ed58d4818fa0f1a
Mayank <mayankmchandratre@gmail.com>
2025-01-12 13:05:01 +0530

    pdfex

--- Backend/build.js
+++ Backend/build.js
@@ -1,2 +1,4 @@
-import { GlobalWorkerOptions } from "pdfjs-dist";
-GlobalWorkerOptions.workerSrc = "/pdf.worker.mjs";
+import { readFileSync, writeFileSync } from 'fs';
+const dir = 'node_modules/pdf.js-extract/lib/pdfjs/pdf.js';
+const content = readFileSync(dir, { encoding: 'utf-8' });
+writeFileSync(dir, content.replace('"./pdf.worker.js";', `__dirname + "/pdf.worker.js";`))
